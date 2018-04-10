0592a64b0b7bba54c0a6570a4c2dce7a78467ff6
Claudio Ortolina <claudio.ortolina@erlang-solutions.com>
2018-04-10 22:20:55 +0100

    Read data from sqlite

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,12 @@
 module Main where
 
 import Lib
+import System.Environment (getArgs)
 
 main :: IO ()
-main = startApp
+main = do
+    args <- getArgs
+    let subcommand = if (length args > 0) then Just (args !! 0) else Nothing
+    case subcommand of
+        Just "migrate" -> runMigrations
+        _ -> startApp
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,30 +1,45 @@
-{-# LANGUAGE OverloadedStrings     #-}
-{-# LANGUAGE DataKinds             #-}
-{-# LANGUAGE TemplateHaskell       #-}
-{-# LANGUAGE TypeOperators         #-}
-{-# LANGUAGE DuplicateRecordFields #-}
+{-# LANGUAGE DataKinds                  #-}
+{-# LANGUAGE DeriveGeneric              #-}
+{-# LANGUAGE DuplicateRecordFields      #-}
+{-# LANGUAGE FlexibleInstances          #-}
+{-# LANGUAGE GADTs                      #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE MultiParamTypeClasses      #-}
+{-# LANGUAGE OverloadedStrings          #-}
+{-# LANGUAGE QuasiQuotes                #-}
+{-# LANGUAGE TemplateHaskell            #-}
+{-# LANGUAGE TypeFamilies               #-}
+{-# LANGUAGE TypeOperators              #-}
 
 module Lib
   ( startApp
   , app
+  , runMigrations
   ) where
 
+import           Control.Monad.IO.Class (MonadIO, liftIO)
 import           Data.Aeson
 import           Data.Aeson.TH
 import           Data.Aeson.Types         (camelTo2)
+import           Data.Text                (Text)
+import           GHC.Generics
 import           Network.Wai
 import           Network.Wai.Handler.Warp
 import           Network.Wai.Logger       (withStdoutLogger)
 import           Servant
+import           Database.Persist.TH      (mkMigrate, mkPersist, persistLowerCase, share, sqlSettings)
+import           Database.Persist
+import           Database.Persist.Sqlite
 
-data Exam = Exam
-  { id :: Int }
-
-data Patient = Patient
-  { id        :: Int
-  , firstName :: String
-  , lastName  :: String
-  } deriving (Eq, Show)
+share [mkPersist sqlSettings, mkMigrate "migrateAll"] [persistLowerCase|
+Patient
+    firstName Text
+    lastName  Text
+    deriving  Eq Show Generic
+Exam
+    type     Text
+    deriving Eq Show Generic
+|]
 
 concat <$> mapM
   (deriveJSON defaultOptions { fieldLabelModifier = camelTo2 '_' })
@@ -46,12 +61,24 @@ app = serve api server
 api :: Proxy API
 api = Proxy
 
+runDb = runSqlite "data/hypo.db"
+
+runMigrations :: IO ()
+runMigrations =
+    runDb $ do runMigration migrateAll
+
 server :: Server API
-server = return patients
-         :<|> return exams
+server = getPatients :<|> getExams
+    where
+        getPatients = liftIO selectPatients
+        getExams = liftIO selectExams
 
-patients :: [Patient]
-patients = [Patient 1 "Isaac" "Newton", Patient 2 "Albert" "Einstein"]
+selectPatients :: IO [Patient]
+selectPatients = do
+    patientList <- runDb $ selectList [] []
+    return $ map (\(Entity _ u) -> u) patientList
 
-exams :: [Exam]
-exams = [Exam 1, Exam 2, Exam 3]
+selectExams :: IO [Exam]
+selectExams = do
+    examList <- runDb $ selectList [] []
+    return $ map (\(Entity _ u) -> u) examList
